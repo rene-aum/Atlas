7815a9ef8938b28725e0f031c18f6d15d1200816
carlosrene.flores.mendive@bbva.com <mi36597@MI36597MAC.local>
2025-12-03 12:39:32 -0600

    toolbox drive, more changes

--- utils/drive_toolbox.py
+++ utils/drive_toolbox.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import pandas as pd
 import pytz
 from gspread_dataframe import set_with_dataframe,get_as_dataframe
+import io
 
 
 
@@ -26,7 +27,7 @@ def get_last_modification_date_drive(drive,sheet_id):
     data_date = pd.to_datetime(formatted_timestamp).strftime('%Y-%m-%d')
     return data_date
 
-def create_file_in_drive_folder(gc,file_name,folder_id,df_to_set=None):
+def create_sheets_in_drive_folder(gc,file_name,folder_id,df_to_set=None):
 
     spreadsheet = gc.create(file_name, folder_id=folder_id)
     # spreadsheet.share('user@example.com', perm_type='user', role='writer') # Optional: share
@@ -36,7 +37,7 @@ def create_file_in_drive_folder(gc,file_name,folder_id,df_to_set=None):
         set_with_dataframe(worksheet, df_to_set)
     print(f"Google Sheet {file_name} created and updated in folder ID: {folder_id}")
 
-def update_file_in_drive_folder(gc,spreadsheet_id,worksheet_name,df_to_update):
+def update_sheets_in_drive_folder(gc,spreadsheet_id,worksheet_name,df_to_update):
 
     try:
         # 1. Open the existing spreadsheet by ID
@@ -78,3 +79,46 @@ def list_file_ids_for_drive_folder(drive, folder_id:str):
         file_id_dict[file['title']] = file['id']
     return file_id_dict
 
+def read_csv_from_drive(drive,file_id):
+    """
+    """
+    file = drive.CreateFile({'id': file_id})
+    csv_bytes = file.GetContentString()  # returns CSV as a text string
+
+    # --- Load into pandas ---
+    df = pd.read_csv(io.StringIO(csv_bytes))
+    return df
+
+def write_csv_to_drive(drive,file_id, df):
+    """Ya debe existir el archivo csv en drive y por tanto el file_id
+        df: pandas dataframe
+    """
+    # Convert to CSV
+    csv_buffer = io.StringIO()
+    df.to_csv(csv_buffer, index=False)
+    csv_buffer.seek(0)
+
+    # Load existing Drive file by ID
+    file = drive.CreateFile({"id": file_id})
+    file.SetContentString(csv_buffer.getvalue())
+    file.Upload()    # <-- overwrites content, keeps same file ID
+
+    print("Updated successfully.")
+
+def create_csv_file_in_drive_folder(drive,folder_id,df,filename):
+    """filename: string with extension .csv
+    """
+    csv_buffer = io.StringIO()
+    df.to_csv(csv_buffer, index=False)
+    csv_buffer.seek(0)
+    csv_str = csv_buffer.getvalue()
+    file_metadata = {
+                    "title": filename,   # what the user sees in Drive
+                    "mimeType": "text/csv",
+                    "parents": [{"id": folder_id}] 
+                        }
+    file = drive.CreateFile(file_metadata)
+    file.SetContentString(csv_str)  # upload from string
+    file.Upload()
+    print("Uploaded file ID:", file["id"])
+    return file["id"]
