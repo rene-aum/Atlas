9cc368e25349b7937e0b61339ca0f8cfeb25c620
Moises Eduardo Jimenez Hidalgo <moiseseduardo.jimenez@bbva.com>
2026-09-14 11:53:22 -0600

    fix para que flag dummy siga considerando las lineas sin booker de origen

--- PipelinesConsumo/src/crm_config.py
+++ PipelinesConsumo/src/crm_config.py
@@ -476,5 +476,6 @@ CRM_REPORTE_CITAS_COLUMNS = [
     'flag_dummy',
     'flag_duplicada',
     'flag_cita_agendada_comprador',
-    'flag_cita_show_comprador'
+    'flag_cita_show_comprador',
+    'flag_booker_nulo'
 ]
