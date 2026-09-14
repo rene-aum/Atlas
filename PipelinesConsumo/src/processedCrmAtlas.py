303f0217085b255dbe03348158b6cb5fdb9d4510
Moisesjimhgo <moiseseduardo.jimenez@bbva.com>
2026-09-14 12:35:27 -0600

    Update processedCrmAtlas.py

--- PipelinesConsumo/src/processedCrmAtlas.py
+++ PipelinesConsumo/src/processedCrmAtlas.py
@@ -919,11 +919,10 @@ class ProcessedCrmAtlas:
 
         print('lineas finales en citas: ',len(citas_cons))
 
-        return citas_cons
-        # self._select_existing_columns(
-        #     citas_cons,
-        #     self._dedupe_columns(CRM_REPORTE_CITAS_COLUMNS),
-        # )
+        return self._select_existing_columns(
+            citas_cons,
+            self._dedupe_columns(CRM_REPORTE_CITAS_COLUMNS),
+        )
 
 
     def proc_reporte_simulaciones(self,solicitudes_credito):
